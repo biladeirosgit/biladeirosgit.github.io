68ea41d6668240abde53b9889a0b1b3dff94280b
bila-olimpiadas <bilaolimpiadas@gmail.com>
2024-03-24 15:35:57 +0000

    t

--- ranking/script.js
+++ ranking/script.js
@@ -27,8 +27,242 @@ var players = [
 { "#": "25º" ,Nome: "Lekky"     , Pts: 3    , Pr: 1 , Mpp : 3.0  , Tg: 0 },
 ]
 
+var participacoes = 
+    {
+        "Xadas" : {
+            "Bilatrecos" : {
+                "Lugar" : "2º",
+                "Pontos" : 10
+            },
+            "TFT" : {
+                "Lugar" : "3º",
+                "Pontos" : 8
+            },
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Wisdow" : {
+            "Bilatrecos" : {
+                "Lugar" : "1º",
+                "Pontos" : 13
+            },
+            "TFT" : {
+                "Lugar" : "9º",
+                "Pontos" : 3
+            },
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Mestre Gui" : {
+            "Bilatrecos" : {
+                "Lugar" : "1º",
+                "Pontos" : 13
+            },
+            "TFT" : {
+                "Lugar" : "2º",
+                "Pontos" : 10
+            },
+        },
+        "Bárbara" : {
+            "Bilatrecos" : {
+                "Lugar" : "2º",
+                "Pontos" : 10
+            },
+            "TFT" : {
+                "Lugar" : "3º",
+                "Pontos" : 8
+            },
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "Geremias" : {
+            "Bilatrecos" : {
+                "Lugar" : "5º",
+                "Pontos" : 5
+            },
+            "TFT" : {
+                "Lugar" : "6º",
+                "Pontos" : 5
+            },
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Braz" : {
+            "Bilatrecos" : {
+                "Lugar" : "4º",
+                "Pontos" : 6
+            },
+            "TFT" : {
+                "Lugar" : "10º",
+                "Pontos" : 3
+            },
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Squnha" : {
+            "Bilatrecos" : {
+                "Lugar" : "6º",
+                "Pontos" : 5
+            },
+            "TFT" : {
+                "Lugar" : "7º",
+                "Pontos" : 4
+            },
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Cludos" : {
+            "Bilatrecos" : {
+                "Lugar" : "4º",
+                "Pontos" : 6
+            },
+            "TFT" : {
+                "Lugar" : "5º",
+                "Pontos" : 5
+            },
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "Droga" : {
+            "TFT" : {
+                "Lugar" : "1º",
+                "Pontos" : 13
+            }
+        },
+        "Esquilo" : {
+            "Bilatrecos" : {
+                "Lugar" : "5º",
+                "Pontos" : 5
+            },
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Rodry" : {
+            "Bilatrecos" : {
+                "Lugar" : "6º",
+                "Pontos" : 5
+            },
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Filipe" : {
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Lemos" : {
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Sardoal" : {
+            "Futbiladas" : {
+                "Lugar" : "1º",
+                "Pontos" : 8
+            },
+        },
+        "Simão" : {
+            "Bilatrecos" : {
+                "Lugar" : "3º",
+                "Pontos" : 8
+            },
+        },
+        "Camilo" : {
+            "Bilatrecos" : {
+                "Lugar" : "3º",
+                "Pontos" : 8
+            },
+        },
+        "Hydra" : {
+            "TFT" : {
+                "Lugar" : "8º",
+                "Pontos" : 4
+            },
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "João Nuno" : {
+            "TFT" : {
+                "Lugar" : "9º",
+                "Pontos" : 3
+            },
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "Serino" : {
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "Areias" : {
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "Tone" : {
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "Rui" : {
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "Costa" : {
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "Zé Afonso" : {
+            "Futbiladas" : {
+                "Lugar" : "2º",
+                "Pontos" : 4
+            },
+        },
+        "Lekky" : {
+            "TFT" : {
+                "Lugar" : "10º",
+                "Pontos" : 3
+            },
+        }
+    }
+
+function showHideRow(row) { 
+    $("#" + row).toggle(); 
+} 
+
 // Função para criar a tabela
-function criarTabela(players) {
+function criarTabela(players,participacoes) {
     var tabela = document.getElementById("rankingBody");
 
     // Limpar tabela existente
@@ -41,10 +275,31 @@ function criarTabela(players) {
         th.textContent = prop.replace(/_/g, ' ');
         headerRow.appendChild(th);
     });
-
+    var i = 0
     // Preencher a tabela com os jogadores ordenados
     players.forEach(function(player, index) {
         var row = tabela.insertRow();
+        i+=1
+        id = "hidden_row"+i
+        row.setAttribute( "onClick", "showHideRow('"+id+"');" );
+        
+        // Hidden
+        var rowHidden = tabela.insertRow();
+        rowHidden.id = "hidden_row"+i
+        rowHidden.className = "hidden_row"
+        var cellHidden = rowHidden.insertCell();
+        cellHidden.setAttribute("colspan",6);
+        cellHidden.innerHTML = "<p><b>Participações</b></p>";
+        torneios_participados = participacoes[player.Nome]
+        for (let torneio in torneios_participados){
+            cellHidden.innerHTML += `
+            <p>
+            <b>Torneio</b> : ${torneio}<br>
+            <b>Lugar</b> : ${torneios_participados[torneio]["Lugar"]}<br>
+            <b>Pontos Ganhos</b> : +${torneios_participados[torneio]["Pontos"]}</br>
+            </p>
+            `
+        }
 
         var cellPos = row.insertCell();
         cellPos.textContent = player["#"]
@@ -71,4 +326,4 @@ function criarTabela(players) {
 
 
 // Criar tabela inicialmente
-criarTabela(players);
+criarTabela(players,participacoes);
